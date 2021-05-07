e312f62f8ece26187bdac54bb6016f37a45bf4ad
RaianNolaco <81272272+RaianNolaco@users.noreply.github.com>
2021-05-07 12:17:21 -0300

    Create Banco_tabela_003_ALTER_TABLE.sql

---
+++ Banco_tabela_003_ALTER_TABLE.sql
@@ -0,0 +1,21 @@
+use cadastro;
+
+CREATE TABLE cusos(
+  nome       varchar(30) not null unique, 
+  discricao  text,
+  carga      int unsigned,
+  totalaulas int unsigned,
+  ano        year default '2021'
+)DEFAULT CHARSET = utf8;
+
+ALTER TABLE cusos
+RENAME TO cursos;
+
+ALTER TABLE cursos
+ADD COLUMN id_curso int first;
+
+desc cursos;
+
+#add  chave primaria
+ALTER TABLE cursos
+ADD PRIMARY KEY(id_curso);
