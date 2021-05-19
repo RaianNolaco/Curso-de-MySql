104199279506de1605ecf9b88c4ca09ba27f8d14
RaianNolaco <81272272+RaianNolaco@users.noreply.github.com>
2021-05-19 01:34:20 -0300

    left join e right join

--- Banco_de_dados_013_TESTES_JOINS.sql
+++ Banco_de_dados_013_TESTES_JOINS.sql
@@ -21,9 +21,26 @@ insert into TabelaB values
 ('Carlos'),('Manoel'),
 ('Luiz'),('Fernando');
 
-/*Inner join = retorna os registros em comum entre as duas tabelas.
+/*Inner join: retorna os registros em comum entre as duas tabelas.
   No exemplo abaixo quero ver os nomes semelhantes nas duas tabelas*/
 Select a.nome,b.nome
-FROM TabelaA as A
+FROM TabelaA as a
 inner join TabelaB as b
+on a.nome = b.nome;
+
+/*left join: retorna todos os registros da tabela que está a esquerda do 
+inner(coloque em uma linha para ver qual e da esquerda ou direita)porem mesmo assim
+contina retornado os que estão em comum entre as duas tabelas porem tera todos 
+os registros que tem na tabela da esquerda*/
+select a.nome,b.nome
+from TabelaA as a
+left join TabelaB as b
+on a.nome = b.nome;
+
+/*right join: segue a mesma logica do left join porem 
+o right joinn faz com o lado direito, seleciona todos em comum e todos 
+que tem no lado  direito do join*/
+select a.nome,b.nome
+from TabelaA as a
+right join TabelaB as b
 on a.nome = b.nome;
