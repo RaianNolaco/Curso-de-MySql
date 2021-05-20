b62742f4fd626b32a96f1fe2c24b88f8c5f3c61c
RaianNolaco <81272272+RaianNolaco@users.noreply.github.com>
2021-05-20 00:45:49 -0300

    Update Banco_de_dados_14_JOINS_COM_VARIAS_TABELAS.sql

--- Banco_de_dados_14_JOINS_COM_VARIAS_TABELAS.sql
+++ Banco_de_dados_14_JOINS_COM_VARIAS_TABELAS.sql
@@ -26,3 +26,19 @@ insert into gafanhoto_assiste_curso value
 (default,'2015-12-22','3','6'),
 (default,'2014-01-01','22','12'),
 (default,'2016-05-12','1','19');
+
+/*fazendp um select na tabela gafanhotos e gafanhoto_assiste_curso 
+usando o join para juntar as duas tabelas em apenas 1 select
+o AS serve para apelidar a tabela */
+select  g.nome, idcurso from gafanhotos as g 
+join gafanhoto_assiste_curso as a
+on g.id = a.idgafanhoto
+order by g.nome;
+
+/*Aqui temos um join com 3 tabelas , gafanhotos,cursos e a tabela filho entre elas
+desta forma podemos fazer um select mais claro e mais objetivo*/
+select g.nome,c.nome from gafanhotos as g
+join gafanhoto_assiste_curso as a
+on g.id = a.idgafanhoto
+join cursos as c
+on a.idcurso = c.idcurso;
